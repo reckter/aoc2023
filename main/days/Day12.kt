7bb360a1df3d5e2808a8943d5465836fd28e48be
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2023-12-17 14:59:01 +0100

    Solve Day 12 part 2

--- main/days/Day12.kt
+++ main/days/Day12.kt
@@ -1,11 +1,11 @@
 package me.reckter.aoc.days
 
 import me.reckter.aoc.Day
+import me.reckter.aoc.days.Day12.Tile.Broken
+import me.reckter.aoc.days.Day12.Tile.Operational
 import me.reckter.aoc.days.Day12.Tile.Unknown
-import me.reckter.aoc.nullIfEmpty
 import me.reckter.aoc.solution
 import me.reckter.aoc.solve
-import me.reckter.aoc.splitAt
 import me.reckter.aoc.toIntegers
 
 class Day12 : Day {
@@ -17,66 +17,65 @@ class Day12 : Day {
         Unknown,
     }
 
-    fun getAllPossibleSpringsForDeterminedSpring(
+    fun countAllPossibleSpringsForDeterminedSpring(
         springs: List<Tile>,
         first: Tile,
         nextRuns: List<Int>,
         currentRun: Int?,
-    ): List<List<Tile>>? {
+    ): Long {
         if (first == Tile.Unknown) {
             error("got unknown tile")
         }
         if (currentRun == null) {
             if (first == Tile.Broken) {
-                if (nextRuns.isEmpty()) return null
-                return getAllPossibleSprings(springs.drop(1), nextRuns.drop(1), nextRuns.first() - 1)
-                    ?.map { listOf(first) + it }
+                if (nextRuns.isEmpty()) return 0
+                return countAllPossibleSprings(springs.drop(1), nextRuns.drop(1), nextRuns.first() - 1)
             } else {
-                return getAllPossibleSprings(springs.drop(1), nextRuns, null)
-                    ?.map { listOf(first) + it }
+                return countAllPossibleSprings(springs.drop(1), nextRuns, null)
             }
         } else {
             if (first == Tile.Operational) {
-                if (currentRun > 0) return null
-                return getAllPossibleSprings(springs.drop(1), nextRuns, null)
-                    ?.map { listOf(first) + it }
+                if (currentRun > 0) return 0
+                return countAllPossibleSprings(springs.drop(1), nextRuns, null)
             } else {
-                return getAllPossibleSprings(springs.drop(1), nextRuns, currentRun - 1)
-                    ?.map { listOf(first) + it }
+                if (currentRun == 0) return 0
+                return countAllPossibleSprings(springs.drop(1), nextRuns, currentRun - 1)
             }
         }
     }
 
-    fun getAllPossibleSprings(
+    val cache = mutableMapOf<Triple<List<Tile>, List<Int>, Int?>, Long>()
+
+    fun countAllPossibleSprings(
         springs: List<Tile>,
         nextRuns: List<Int>,
         currentRun: Int?,
-    ): List<List<Tile>>? {
+    ): Long {
         if (springs.isEmpty()) {
-            if ((currentRun != null && currentRun != 0) || nextRuns.isNotEmpty()) return null
-            return listOf(emptyList())
+            if ((currentRun != null && currentRun != 0) || nextRuns.isNotEmpty()) return 0
+            return 1
+        }
+
+        val key = Triple(springs, nextRuns, currentRun)
+        if (cache.containsKey(key)) {
+            return cache[key]!!
         }
         val first = springs.first()
         if (first != Tile.Unknown) {
-            return getAllPossibleSpringsForDeterminedSpring(springs, first, nextRuns, currentRun)
+            val ret = countAllPossibleSpringsForDeterminedSpring(springs, first, nextRuns, currentRun)
+            cache[key] = ret
+            return ret
         }
 
-        return listOf(
-            Tile.Operational,
-            Tile.Broken,
-        )
-            .mapNotNull { replaceFirst ->
-                getAllPossibleSpringsForDeterminedSpring(springs, replaceFirst, nextRuns, currentRun)
+        val ret =
+            listOf(
+                Operational,
+                Broken,
+            ).sumOf { replaceFirst ->
+                countAllPossibleSpringsForDeterminedSpring(springs, replaceFirst, nextRuns, currentRun)
             }
-            .flatten()
-            .nullIfEmpty()
-    }
-
-    fun countBrokenRuns(springs: List<Tile>): List<Int> {
-        return springs
-            .splitAt { it == Tile.Operational }
-            .map { it.count() }
-            .filter { it > 0 }
+        cache[key] = ret
+        return ret
     }
 
     override fun solvePart1() {
@@ -94,37 +93,19 @@ class Day12 : Day {
                         }
                     }
 
-                getAllPossibleSprings(springs, numbers, null)
-                    ?.count()
-                    ?: error("no solution found")
+                countAllPossibleSprings(springs, numbers, null)
             }
             .sum()
             .solution(1)
     }
 
-    fun getAllPermutations(list: List<Tile>): List<List<Tile>> {
-        if (list.isEmpty()) return listOf(emptyList())
-        val first = list.first()
-        val prefix =
-            if (first == Tile.Unknown) {
-                listOf(Tile.Operational, Tile.Broken)
-            } else {
-                listOf(first)
-            }
-        val restPermutations = getAllPermutations(list.drop(1))
-
-        return prefix
-            .flatMap { pre ->
-                restPermutations.map {
-                    listOf(pre) + it
-                }
-            }
-    }
-
     override fun solvePart2() {
-        loadInput(1)
+        loadInput()
             .map {
                 val (field, numbersStr) = it.split(" ")
+                "$field?$field?$field?$field?$field" to "$numbersStr,$numbersStr,$numbersStr,$numbersStr,$numbersStr"
+            }
+            .map { (field, numbersStr) ->
                 val numbers = numbersStr.split(",").toIntegers()
                 val springs =
                     field.map {
@@ -135,34 +116,8 @@ class Day12 : Day {
                             else -> error("invalid tile $it")
                         }
                     }
-                springs to numbers
-            }
-            .map { (springs, numbers) ->
-                val withoutConnection =
-                    getAllPossibleSprings(springs, numbers, null)
-                        ?.map { countBrokenRuns(it) }
-                        ?.count { it == numbers } ?: error("no solution found")
 
-                val connections =
-                    getAllPermutations(
-                        listOf(
-                            Tile.Unknown,
-                            Tile.Unknown,
-                            Tile.Unknown,
-                            Tile.Unknown,
-                        ),
-                    )
-                val ret =
-                    connections
-                        .map {
-                            val wholeSprings = it.map { springs + listOf(it) }.flatten() + springs
-                            val wholeNumbers = it.map { numbers }.flatten() + numbers
-                            getAllPossibleSprings(wholeSprings, wholeNumbers, null)
-                                ?.count() ?: 0
-                        }
-                println("$ret $withoutConnection")
-
-                ret.sum()
+                countAllPossibleSprings(springs, numbers, null)
             }
             .sum()
             .solution(2)
